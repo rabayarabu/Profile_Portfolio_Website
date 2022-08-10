27d33e73688772b94feee81cd1fc0ce5490b94d7
rabayarabu <70410637+rabayarabu@users.noreply.github.com>
2022-08-11 01:26:37 +0600

    greet.js for learning props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import Greet from './pages/Greet';
 import Name from './pages/Name';
 import Contact from './pages/Contact';
 import About from './pages/About';
@@ -13,6 +14,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <>
+    <Greet name="rabu"/>
     <Router>
       <Routes>
         <Route path="/" element={<Name />} />
---
+++ src/pages/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Greet (props) {
+    console.log(props)
+    return(
+        
+        <h1>Hello {props.name}</h1>
+    )
+}
+
+export default Greet
